3ebe20253511f00381167d3292b58297d7203f94
You Feng <1513544368@qq.com>
2025-01-28 21:16:46 +0800

    Add files via upload

---
+++ HK32F030M_Project/Objects/protocol.d
@@ -0,0 +1,12 @@
+.\objects\protocol.o: src\protocol.c
+.\objects\protocol.o: .\inc\wifi.h
+.\objects\protocol.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\protocol.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\objects\protocol.o: .\inc\protocol.h
+.\objects\protocol.o: .\inc\system.h
+.\objects\protocol.o: .\inc\mcu_api.h
+.\objects\protocol.o: .\inc\led.h
+.\objects\protocol.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\protocol.o: .\inc\delay.h
+.\objects\protocol.o: .\inc\motor.h
+.\objects\protocol.o: .\inc\beep.h
---
+++ HK32F030M_Project/Objects/system.d
@@ -0,0 +1,7 @@
+.\objects\system.o: src\system.c
+.\objects\system.o: .\inc\wifi.h
+.\objects\system.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\system.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\objects\system.o: .\inc\protocol.h
+.\objects\system.o: .\inc\system.h
+.\objects\system.o: .\inc\mcu_api.h
